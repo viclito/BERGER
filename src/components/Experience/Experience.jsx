adf752f7b8ee4f3c34441d12720904a2e42feae4
viclito <berglin1998@gmail.com>
2024-01-28 21:48:38 +0530

    altering Experience

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -31,7 +31,8 @@ export const Experience = () => {
                   alt={`${historyItem.organisation} Logo`}
                 />
                 <div className={styles.historyItemDetails}>
-                  <h3>{`${historyItem.role}, ${historyItem.organisation}`}</h3>
+                  <h3>{historyItem.role}</h3>
+                  <h4> {historyItem.organisation}</h4>
                   <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
                   <ul>
                     {historyItem.experiences.map((experience, id) => {
